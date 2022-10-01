a6686e79390bcf9e4b7b428d6abb7922a3c40553
Ikhlas <ikhlaskhan007@gmail.com>
2022-10-02 01:01:36 +0400

    change constructor to payable

--- exercises/gas/contracts/Gas.sol
+++ exercises/gas/contracts/Gas.sol
@@ -2,62 +2,65 @@
 pragma solidity 0.8.17;
 
 contract GasContract {
-    uint public immutable totalSupply; // cannot be updated
+    uint256 public immutable totalSupply; // cannot be updated
     address[5] public administrators;
 
     struct Payment {
-        uint paymentType;
-        uint paymentID;
-        uint amount;
+        uint256 paymentType;
+        uint256 paymentID;
+        uint256 amount;
     }
     struct ImportantStruct {
-        uint valueA; // max 3 digits
-        uint bigValue;
-        uint valueB; // max 3 digits
+        uint256 valueA; // max 3 digits
+        uint256 bigValue;
+        uint256 valueB; // max 3 digits
     }
 
-    event Transfer(address recipient, uint amount);
+    event Transfer(address, uint256);
 
-    mapping(address => uint) balances;
+    mapping(address => uint256) balances;
     mapping(address => Payment[]) payments;
-    mapping(address => uint) public whitelist;
+    mapping(address => uint256) public whitelist;
 
-    constructor(address[5] memory _admins, uint _totalSupply) {
-        totalSupply = _totalSupply;
+    constructor (address[5] memory _admins, uint256 _totalSupply) payable{
         administrators = _admins;
-        balances[msg.sender] = totalSupply;
+        totalSupply = _totalSupply;
+        balances[msg.sender] = _totalSupply;
     }
 
     function transfer(
         address _recipient,
-        uint _amount,
+        uint256 _amount,
         string calldata _name
-    ) external { unchecked{
-        balances[_recipient] += _amount;}
+    ) external {
+        unchecked {
+            balances[_recipient] += _amount;
+        }
         emit Transfer(_recipient, _amount);
         payments[msg.sender].push(Payment(1, 0, _amount));
     }
 
-    function balanceOf(address _user) external view returns (uint) {
+    function balanceOf(address _user) external view returns (uint256) {
         return balances[_user];
     }
 
     function updatePayment(
         address _user,
-        uint _ID,
-        uint _amount,
-        uint _type
+        uint256 _ID,
+        uint256 _amount,
+        uint256 _type
     ) external {
-        unchecked{
-        for (uint i = 0; i < 6; ++i) {
-            if (administrators[i] == msg.sender) {
-                payments[_user][0].paymentType = _type;
-                payments[_user][0].amount = _amount;
-                return;
+        unchecked {
+            for (uint256 i = 0; i < 6; ++i) {
+                if (administrators[i] == msg.sender) {
+                    payments[_user][0].paymentType = _type;
+                    payments[_user][0].amount = _amount;
+                    return;
+                }
             }
+            revert();
         }
-        revert();
-    }}
+    }
 
     function getPayments(address _user)
         external
@@ -71,18 +74,19 @@ contract GasContract {
         return true;
     }
 
-    function addToWhitelist(address _userAddrs, uint _tier) external {
+    function addToWhitelist(address _userAddrs, uint256 _tier) external {
         whitelist[_userAddrs] = _tier;
     }
 
     function whiteTransfer(
         address _recipient,
-        uint _amount,
+        uint256 _amount,
         ImportantStruct calldata _struct
-    ) external { 
+    ) external {
         unchecked {
-        _amount -= whitelist[msg.sender];
-        balances[msg.sender] -= _amount;
-        balances[_recipient] += _amount;
-    }}
+            _amount -= whitelist[msg.sender];
+            balances[msg.sender] -= _amount;
+            balances[_recipient] += _amount;
+        }
+    }
 }
