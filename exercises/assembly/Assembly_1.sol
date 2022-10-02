de3935733da2e0b1cbb73045044e4c162260b13b
hasanza <hr90@student.london.ac.uk>
2022-10-03 01:37:05 +0500

    Completed assignment 1

--- exercises/assembly/Assembly_1.sol
+++ exercises/assembly/Assembly_1.sol
@@ -3,15 +3,15 @@ pragma solidity ^0.8.4;
 contract Intro {
     function intro() public pure returns (uint16) {
         uint256 mol = 420;
-
         // Yul assembly magic happens within assembly{} section
         assembly {
-            // stack variables are instantiated with
-            // let variable_name := VALßUE
             // instantiate a stack variable that holds the value of mol
-            // To return it needs to be stored in memory
-            // with command mstore(MEMORY_LOCATION, STACK_VARIABLE)
-            // to return you need to specify address and the size from the starting point
+            let mol_val := mol 
+            // store to memory with command mstore(MEMORY_LOCATION, STACK_VARIABLE)
+            mstore(0x80, mol_val)
+            //return(offset, size)
+            // return the value starting from mem slot 0x80, running 32 bytes (the whole slot)
+            return(0x80, 32)
         }
     }
 }
