c93bc92984354dc6884a4c40a3dffd2959fc9072
hasanza <hr90@student.london.ac.uk>
2022-10-03 01:49:48 +0500

    Completed assignment 3

--- exercises/assembly/Assembly_3.sol
+++ exercises/assembly/Assembly_3.sol
@@ -6,7 +6,14 @@ contract SubOverflow {
     function subtract(uint256 x, uint256 y) public pure returns (uint256) {
         // Write assembly code that handles overflows
         assembly {
-
+            // If y is greater than x, return 0
+            if gt(y, x) { return(0,0) }
+            // otherwise return x - y
+            let res := sub(x, y)
+            // load into memory
+            mstore(0x80, res)
+            // return
+            return(0x80, 32)
         }
     }
 }
